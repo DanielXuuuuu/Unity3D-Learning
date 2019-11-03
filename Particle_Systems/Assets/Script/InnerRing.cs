962f6dbaee8312793bd761e4c73c4a85d8397568
Daniel <979223119@qq.com>
2019-11-03 20:35:25 +0800

    hw7 - 粒子光环

---
+++ Particle_Systems/Assets/Script/InnerRing.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerRing : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    private ParticleSystem.Particle[] particles;
+    private int particleNum = 3000;
+    private float[] particleAngle;
+    private float[] particleRadius;
+    private float[] nonCollectRadius;
+    private float[] collectRadius;
+    private float minRadius = 3.3f, maxRadius = 4.2f;
+
+    public float speed = 0.2f;
+    public float collectSpeed = 3f;
+    public bool isCollected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = this.GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[particleNum];
+        particleAngle = new float[particleNum];
+        particleRadius = new float[particleNum];
+        nonCollectRadius = new float[particleNum];
+        collectRadius = new float[particleNum];
+        particleSystem.maxParticles = particleNum;
+        particleSystem.Emit(particleNum);
+        particleSystem.GetParticles(particles);
+        for (int i = 0; i < particleNum; i++)
+        {
+            float midR = (maxRadius + minRadius) / 2;
+            //最小半径随机扩大
+            float rate1 = Random.Range(1.0f, midR / minRadius);
+            //最大半径随机缩小
+            float rate2 = Random.Range(midR / maxRadius, 1.0f);
+            float radius = Random.Range(minRadius * rate1, maxRadius * rate2);
+
+            float shiftMinAngle = Random.Range(0, 135);
+            float shiftMaxAngle = Random.Range(135, 180);
+            float angle = Random.Range(shiftMinAngle, shiftMaxAngle);
+            if (Random.Range(0, 100) < 50)
+                angle += 180.0f;
+
+            float rad = angle / 180 * Mathf.PI;
+            particles[i].position = new Vector3(radius * Mathf.Cos(rad), radius * Mathf.Sin(rad), 0.0f);
+            particleRadius[i] = radius;
+            particleAngle[i] = angle;
+
+            nonCollectRadius[i] = radius;
+            collectRadius[i] = radius - 1.5f * (radius / minRadius);
+        }
+        particleSystem.SetParticles(particles, particleNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < particleNum; i++)
+        {
+            if (isCollected)
+            {
+                if (particleRadius[i] > collectRadius[i])
+                {
+                    particleRadius[i] -= collectSpeed * (particleRadius[i] / collectRadius[i]) * Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (particleRadius[i] < nonCollectRadius[i])
+                {
+                    particleRadius[i] += collectSpeed * (nonCollectRadius[i] / particleRadius[i]) * Time.deltaTime;
+                }
+                else if (particleRadius[i] > nonCollectRadius[i])
+                {
+                    particleRadius[i] = particleRadius[i];
+                }
+            }
+            particleAngle[i] += Random.Range(0, speed);
+            float rad = particleAngle[i] / 180 * Mathf.PI;
+            particles[i].position = new Vector3(particleRadius[i] * Mathf.Cos(rad), particleRadius[i] * Mathf.Sin(rad), 0.0f);
+        }
+        particleSystem.SetParticles(particles, particleNum);
+    }
+}
---
+++ Particle_Systems/Assets/Script/UserGUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+    private OuterRing[] outerRing;
+    private InnerRing[] innerRing;
+
+    private void Start()
+    {
+        outerRing = GetComponentsInChildren<OuterRing>() as OuterRing[];
+        innerRing = GetComponentsInChildren<InnerRing>() as InnerRing[];
+    }
+    private void OnGUI()
+    {
+        GUIStyle button_style;
+        button_style = new GUIStyle("button")
+        {
+            fontSize = 15
+        };
+        if(GUI.Button(new Rect(Screen.width - 150, Screen.height - 100, 100, 30), "收", button_style))
+        {
+            outerRing[0].isCollected = true;
+            innerRing[0].isCollected = true;
+        }
+        if(GUI.Button(new Rect(Screen.width - 150, Screen.height - 50, 100, 30), "散", button_style))
+        {
+            outerRing[0].isCollected = false;
+            innerRing[0].isCollected = false;
+        }
+    }
+}
