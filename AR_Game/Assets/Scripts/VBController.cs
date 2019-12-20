38e66718e43a7b7b20bb495aa241d606fbb0e2dc
Daniel <979223119@qq.com>
2019-12-20 09:29:48 +0800

    hw10 - AR虚拟按键小游戏

---
+++ AR_Game/Assets/Scripts/VBController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBController : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject dragon;
+    public Vector3 target = new Vector3(-0.2f, 0, -0.2f);
+    public float speed = 0.1f;
+    public bool pressed = false;
+
+    void Start()
+    {
+        //在所有子物体类中找到所有VirtualButtonBehaviour组件
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        //在虚拟按钮中注册TrackableBehaviour事件
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+
+        dragon = transform.Find("dragon").gameObject;
+    }
+    
+    void Update()
+    {
+        dragon.transform.localPosition = Vector3.MoveTowards(dragon.transform.localPosition, target, speed * Time.deltaTime);
+        if (dragon.transform.localPosition == target)
+        {
+            target = new Vector3(-0.2f, 0, -0.2f);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log(vb.VirtualButtonName + " btn pressed");
+        if(pressed == false)
+        {
+            pressed = true;
+            target = new Vector3(-0.2f, 0.4f, -0.2f);
+        }
+        
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log(vb.VirtualButtonName + " btn released");
+        pressed = false;
+    }
+}
---
+++ AR_Game/Assets/Scripts/birdGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdGenerator : MonoBehaviour
+{
+    private GameObject bird;
+    // Use this for initialization
+    void Start()
+    {
+        bird = Object.Instantiate(Resources.Load<GameObject>("bird"),
+                    Vector3.zero, Quaternion.identity, null);
+        bird.transform.parent = this.transform;
+        bird.transform.localPosition = new Vector3(-0.2f, 0.1f, 0.5f);
+        bird.transform.localScale *= 0.07f;
+        bird.transform.Rotate(0, 180, 0, Space.Self); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bird.transform.localPosition -= new Vector3(0, 0, 0.015f);
+        if (bird.transform.localPosition.z < -0.4)
+        {
+            bird.transform.localPosition = new Vector3(-0.2f, 0, 0.5f);
+        }   
+    }
+}
